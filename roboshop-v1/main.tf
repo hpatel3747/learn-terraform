439b6c3f70d1bb1b273dc08c1c9d90715709ca95
Hitesh Patel <hpatel3747@gmail.com>
2024-10-18 12:40:58 -0400

    Learn terraform

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,3 +1,8 @@
 resource "aws_instance" "frontend" {
   ami = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids =  [sg-0f538d9ce9c907af2]
+  tags = {
+    Name = "frontend"
+  }
 }
