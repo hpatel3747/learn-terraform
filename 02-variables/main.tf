d956f170ae022f5b3ef513f9f159150287816869
Hitesh Patel <hpatel3747@gmail.com>
2024-10-23 16:45:58 -0400

    Documentation

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -62,21 +62,24 @@ output "list1_1" {
 
 output "map1_aws_trainer" {
   value = var.map1["aws"]["trainer"]
-  #value = var.map1["aws"].trainer
-}
+  }
 
 # Declare a empty variable and we can get the value from CLI, Unlike ansible or shell we must and should declare a variable in order to use it.
 variable "trainer_name" {}
+
+
+# when you apply terraform, it will prompt for variable value on cli
+# or you can provide variable value on terraform apply argument e.g
+# git pull ; terraform init ; terraform apply -auto-approve -var trainer_name=John
+#
 output "trainer_name" {
   value = var.trainer_name
 }
+# you can set your own variable file e.g test.tfvars set variable value in that file and then provide var file name
+# git pull ; terraform  init ; terraform apply -auto-approve -var-file test.tfvars
+# if you create test.auto.tfvar file it will automatically pickup from that file
+# git pull ; terraform init ; terraform apply -auto-approve # it will pick from auto.tvars automatically
+# the auto.tfvars should be in the directory as main.tf
 
-variable "testV" {
-  default = "test test"
-}
-
-output "test" {
-  value = var.testV
-}
 
 
