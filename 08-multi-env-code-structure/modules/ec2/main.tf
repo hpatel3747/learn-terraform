d3d3ba7b67b5ba60accde09689cae70d151f0ac4
Hitesh Patel <hpatel3747@gmail.com>
2024-11-14 12:34:40 -0500

    updated values relevante to hptldevops

--- 08-multi-env-code-structure/modules/ec2/main.tf
+++ 08-multi-env-code-structure/modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  vpc_security_group_ids = ["sg-06f2d2482af60d937"]
   tags = {
     Name = "test-${var.env}"
   }
