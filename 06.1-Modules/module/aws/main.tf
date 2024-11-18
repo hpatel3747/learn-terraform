b3c9994ea8195973030ed7b98713bea9a33dd2b9
Hitesh Patel <hpatel3747@gmail.com>
2024-11-18 14:35:49 -0500

    updated ansible code to deploy roboshop in docker container

--- 06.1-Modules/module/aws/main.tf
+++ 06.1-Modules/module/aws/main.tf
@@ -9,6 +9,39 @@ resource "aws_vpc" "hptl-vpc-01" {
   }
 }
 
+resource "aws_subnet" "pub-subnet-01" {
+  vpc_id     = aws_vpc.hptl-vpc-01.id
+  cidr_block = "10.0.0.0/26"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "pub-subnet-01"
+  }
+}
+resource "aws_subnet" "pub-subnet-02" {
+  vpc_id     = aws_vpc.hptl-vpc-01.id
+  cidr_block = "10.0.0.64/26"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "pub-subnet-02"
+  }
+}
+resource "aws_subnet" "pri-subnet-01" {
+  vpc_id     = aws_vpc.hptl-vpc-01.id
+  cidr_block = "10.0.0.128/26"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "pri-subnet-01"
+  }
+}
+resource "aws_subnet" "pri-subnet-02" {
+  vpc_id     = aws_vpc.hptl-vpc-01.id
+  cidr_block = "10.0.0.192/26"
+  availability_zone = "us-east-1d"
+  tags = {
+    Name = "pri-subnet-02"
+  }
+}
+
 resource "aws_internet_gateway" "hptl-gw" {
   vpc_id = aws_vpc.hptl-vpc-01.id
 
